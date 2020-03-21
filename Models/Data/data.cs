fd7d77b43be828e0b3859a492fe53e2fc05dde57
Avinash Singh <avinash.singh@nathcorp.com>
2020-03-21 10:58:51 +0530

    Added input field in amt

---
+++ Models/Data/data.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using AMT_Asset__Mgt_Tool_.Models.Data;
+
+namespace AMT_Asset__Mgt_Tool_.Models.Data
+{
+    public class MVC_AMT_data : DbContext
+    {
+        public MVC_AMT_data (DbContextOptions<MVC_AMT_data> options)
+            :base(options)
+        {
+
+        }
+        public DbSet<AMT> AMT { get; set; }
+    }
+}
---
+++ Models/login.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AMT_Asset__Mgt_Tool_.Models
+{
+    public class login
+    {
+        public int Id { get; set; }
+        public string user { get; set; }
+        public string password { get; set; }
+        public string role { get; set; }
+
+    }
+}
